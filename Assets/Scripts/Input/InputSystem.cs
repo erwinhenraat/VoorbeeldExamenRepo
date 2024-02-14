de764e42e435dce844572f8092681b9f0b814b6e
Minoord <70703952+Minoord@users.noreply.github.com>
2024-02-14 11:42:14 +0100

    Added Inputsystem script

---
+++ Assets/Scripts/Input/InputSystem.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine.AddressableAssets;
+using UnityEngine.InputSystem;
+
+public class InputSystem
+{
+    private InputActions _inputAction;
+    private Dictionary<System.Action<InputAction.CallbackContext>, InputAction> _subscribedInputs = new();
+
+    public void InitializeInputSystem() => _inputAction = Addressables.LoadAssetAsync<InputActions>("InputActions").Result;
+
+    public InputAction TryGetAction(string actionName)
+    {
+        InputAction action = _inputAction.FindAction(actionName);
+
+        return action; 
+    }
+
+    public void SubscribeToAction(InputAction inputAction, System.Action<InputAction.CallbackContext> function)
+    {
+        inputAction.Enable();
+        inputAction.performed += function;
+
+        _subscribedInputs.Add(function, inputAction);
+    }
+
+    public void SubscribeToAction(string inputName, System.Action<InputAction.CallbackContext> function, out InputAction inputAction)
+    {
+        inputAction = TryGetAction(inputName);
+        inputAction.Enable();
+        inputAction.performed += function;
+
+        _subscribedInputs.Add(function, inputAction);
+    }
+
+    public void UnsubscribeToAction(InputAction inputAction, System.Action<InputAction.CallbackContext> function)
+    {
+        inputAction.performed -= function;
+
+        _subscribedInputs.Remove(function, out inputAction);
+    }
+
+    public void UnsubscribeToAction(string inputName, System.Action<InputAction.CallbackContext> function)
+    {
+        InputAction inputAction = TryGetAction(inputName);
+        inputAction.performed -= function;
+
+        _subscribedInputs.Remove(function, out inputAction);
+    }
+
+    public void UnsubscribeToAllActions()
+    {
+        foreach (KeyValuePair<System.Action<InputAction.CallbackContext>, InputAction> input in _subscribedInputs)
+        {
+            input.Value.performed -= input.Key;
+        }
+
+        _subscribedInputs.Clear();
+    }
+}
