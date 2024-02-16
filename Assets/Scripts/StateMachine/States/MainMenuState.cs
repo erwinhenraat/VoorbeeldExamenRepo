8f2f98d72dac635fdfd72d9980812e223fe1cbb4
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2024-02-16 13:40:28 +0100

    Imported GameStateMachine package

---
+++ Assets/Scripts/StateMachine/States/MainMenuState.cs
@@ -0,0 +1,19 @@
+namespace StateMachine
+{
+    public class MainMenuState : State
+    {
+        /// <summary>
+        /// Executes code related to entering the Main Menu state.
+        /// </summary>
+        public override void EnterState()
+        {
+            base.EnterState();
+            SetNextState<GameState>();
+        }
+
+        /// <summary>
+        /// Executes code related to leaving the Main Menu state.
+        /// </summary>
+        public override void ExitState() => base.ExitState();
+    }
+}
