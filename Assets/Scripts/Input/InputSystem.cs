5e4d10496aa64e93a2b6e1071244e97f83e7640c
Minoord <70703952+Minoord@users.noreply.github.com>
2024-02-16 11:51:24 +0100

    Added summaries

--- Assets/Scripts/Input/InputSystem.cs
+++ Assets/Scripts/Input/InputSystem.cs
@@ -8,8 +8,18 @@ namespace UntitledCube.Input
         private static InputActions _inputAction = new();
         private static Dictionary<System.Action<InputAction.CallbackContext>, InputAction> _subscribedInputs = new();
 
+        /// <summary>
+        /// Tries to get an action based on the given name
+        /// </summary>
+        /// <param name="actionName"> The name of the action you want </param>
+        /// <returns>Returns the input action you are looking for if it exists </returns>
         public static InputAction TryGetAction(string actionName) => _inputAction.FindAction(actionName);
 
+        /// <summary>
+        /// Enables the given action and add the function to the preformed
+        /// </summary>
+        /// <param name="inputAction"> The action you want to subscribe to</param>
+        /// <param name="function"> The function you want to have subscribed</param>
         public static void SubscribeToAction(InputAction inputAction, System.Action<InputAction.CallbackContext> function)
         {
             inputAction.Enable();
@@ -18,6 +28,12 @@ namespace UntitledCube.Input
             _subscribedInputs.Add(function, inputAction);
         }
 
+        /// <summary>
+        /// Searches for the input based on the inputName, enables it and add the function to the preformed
+        /// </summary>
+        /// <param name="inputName">The name of the action you want</param>
+        /// <param name="function"> The function you want to have subscribed</param>
+        /// <param name="inputAction"> Gives back the action it subsribed to</param>
         public static void SubscribeToAction(string inputName, System.Action<InputAction.CallbackContext> function, out InputAction inputAction)
         {
             inputAction = TryGetAction(inputName);
@@ -27,6 +43,12 @@ namespace UntitledCube.Input
             _subscribedInputs.Add(function, inputAction);
         }
 
+
+        /// <summary>
+        /// Removes the function from the preformed of the given action
+        /// </summary>
+        /// <param name="inputAction"> The action you want to unsubscribe to</param>
+        /// <param name="function"> The function you want to have unsubscribed</param>
         public static void UnsubscribeToAction(InputAction inputAction, System.Action<InputAction.CallbackContext> function)
         {
             inputAction.performed -= function;
@@ -34,6 +56,11 @@ namespace UntitledCube.Input
             _subscribedInputs.Remove(function, out inputAction);
         }
 
+        /// <summary>
+        /// Searches for the input based on the inputName and removes the function from the preformed
+        /// </summary>
+        /// <param name="inputName">The name of the action you want</param>
+        /// <param name="function"> The function you want to have unsubscribed</param>
         public static void UnsubscribeToAction(string inputName, System.Action<InputAction.CallbackContext> function)
         {
             InputAction inputAction = TryGetAction(inputName);
@@ -42,6 +69,9 @@ namespace UntitledCube.Input
             _subscribedInputs.Remove(function, out inputAction);
         }
 
+        /// <summary>
+        /// Unsubscribes each subscribed function from their actions
+        /// </summary>
         public static void UnsubscribeToAllActions()
         {
             foreach (KeyValuePair<System.Action<InputAction.CallbackContext>, InputAction> input in _subscribedInputs)
