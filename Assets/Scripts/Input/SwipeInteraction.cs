a00a461a584efa13ea6e5a007c9fd24d1448fdde
Minoord <70703952+Minoord@users.noreply.github.com>
2024-02-26 10:00:12 +0100

    removed unnecessary brackets

--- Assets/Scripts/Input/SwipeInteraction.cs
+++ Assets/Scripts/Input/SwipeInteraction.cs
@@ -13,10 +13,7 @@ namespace UntitledCube.Input
         private Vector2 _swipePosition;
         private Vector2 _swipeDirection;
 
-        private void OnEnable()
-        {
-            InputSystem.SubscribeToAction("Swipe", Swipe, out _swipeAction);
-        }
+        private void OnEnable() => InputSystem.SubscribeToAction("Swipe", Swipe, out _swipeAction);
 
         private void Swipe(InputAction.CallbackContext callbackContext)
         {
@@ -24,7 +21,8 @@ namespace UntitledCube.Input
 
             Vector2 _newSwipeDirection = CalculateDirection();
 
-            if (_newSwipeDirection == Vector2.zero) return;
+            if (_newSwipeDirection == Vector2.zero) 
+                return;
 
             if(_swipeDirection != _newSwipeDirection ) 
             {
@@ -40,15 +38,11 @@ namespace UntitledCube.Input
             float x = Mathf.Abs(_swipePosition.x);
             float y = Mathf.Abs(_swipePosition.y);
 
-            if (x > _moveThreshold && x > y)
-            {
+            if (x > _moveThreshold && x > y) 
                 return _swipePosition.x > 0 ? Vector2.right : Vector2.left;
-            }
-            else if (y > _moveThreshold && x < y)
-            {
+            else if (y > _moveThreshold && x < y) 
                 return _swipePosition.y > 0 ? Vector2.up : Vector2.down;
-            }
-
+            
             return Vector2.zero;
         }
 
