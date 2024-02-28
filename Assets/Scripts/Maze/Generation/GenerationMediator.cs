5e566158f932672c6829f35fff019f4b18dfcc0a
Samuel <70766878+swzwij@users.noreply.github.com>
2024-02-28 15:36:54 +0100

    merged generation and gravity shift

---
+++ Assets/Scripts/Maze/Generation/GenerationMediator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UntitledCube.Maze.Generation
+{
+    public class GenerationMediator : MonoBehaviour
+    {
+        [SerializeField] private string _seed;
+        [SerializeField] private Button _button;
+
+        private void OnEnable() => _button.onClick.AddListener(GenerateMaze);
+
+        private void OnDisable() => _button.onClick.RemoveListener(GenerateMaze);
+
+        private void Start() => GenerateMaze();
+
+        /// <summary>
+        /// Generate maze.
+        /// </summary>
+        public void GenerateMaze() => MazeGenerator.Generate(new(6, 6), _seed);
+    }
+}
