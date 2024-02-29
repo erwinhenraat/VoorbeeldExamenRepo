47cc5841c77bb7517aa986b9383cabf1660f629a
WarlockGames12 <47526227+WarlockGames12@users.noreply.github.com>
2024-02-29 13:40:10 +0100

    Modified test Player Movement

--- ProefExamenProject/Assets/Scripts/Car/testplayermovement.cs
+++ ProefExamenProject/Assets/Scripts/Car/testplayermovement.cs
@@ -5,19 +5,14 @@ using UnityEngine;
 public class testplayermovement : MonoBehaviour
 {
     public float speed;
-    void Update()
+    private void Update()
     {
         transform.Translate(0 , 0, speed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.A))
-        {
+        if (UnityEngine.Input.GetKeyDown(KeyCode.A))
             transform.Translate(5f * Time.deltaTime, 0f, 0f);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
+        if (UnityEngine.Input.GetKeyDown(KeyCode.D))
             transform.Translate(-5f * Time.deltaTime, 0f, 0f);
-        }
     }
 
     public float GetSpeed()
