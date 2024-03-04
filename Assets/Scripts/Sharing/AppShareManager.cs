e46cf7b8d3b3d1d3182c411a48671e9c251ff72a
Minoord <70703952+Minoord@users.noreply.github.com>
2024-03-04 13:41:50 +0100

    Can share images with help of nativesharing

--- Assets/Scripts/Sharing/AppShareManager.cs
+++ Assets/Scripts/Sharing/AppShareManager.cs
@@ -30,6 +30,44 @@ public class AppShareManager : MonoBehaviour
 
         string screenshotName = $"HighScore_{DateTime.UtcNow.ToOADate()}.jpg";
         string screenShotPath = Path.Combine(Application.persistentDataPath, screenshotName);
+
+        string seedNumber = "123456";
+        string time = "21 secs";
+        string message = $"I challenge you to beat my time of *{time}* in this seed: {seedNumber}";
+
+        ScreenCapture.CaptureScreenshot(screenshotName, 1);
+
+        removeLater.text = screenShotPath;
+
+        yield return new WaitForEndOfFrame();
+
+        new NativeShare().AddFile(screenShotPath)
+        .SetSubject("Untitled Cube Highscore").SetText(message).SetUrl("https://TotallyARealLinkThatGoesToOurApp")
+        .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+        .Share();
+    }
+}
+   /* [SerializeField] private Image testRemoveLater;
+    [SerializeField] private TextMeshProUGUI removeLater;
+
+    private bool _isFocus;
+    private bool _isProcessing;
+
+    private void OnApplicationFocus(bool focus) => _isFocus = focus;
+
+    public void CallSharePopUp()
+    {
+        if (!_isProcessing)
+            StartCoroutine(ShareScreenshotInAnroid());
+    }
+
+    private IEnumerator ShareScreenshotInAnroid()
+    {
+        _isProcessing = true;
+        yield return new WaitForEndOfFrame();
+
+        string screenshotName = $"HighScore_{DateTime.UtcNow.ToOADate()}.jpg";
+        string screenShotPath = Path.Combine(Application.persistentDataPath, screenshotName);
         string seedNumber = "123456";
         string time = "21 secs";
         string message = $"I challenge you to beat my time of *{time}* in this seed: {seedNumber}" +
@@ -69,25 +107,25 @@ public class AppShareManager : MonoBehaviour
         _isProcessing = false;
     }
 }
+*/
+/*
+        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
 
-    /*
-            AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-            AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+        intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+        intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
 
-            intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-            intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
+        AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+        AndroidJavaClass fileClass = new AndroidJavaClass("java.io.File");
 
-            AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-            AndroidJavaClass fileClass = new AndroidJavaClass("java.io.File");
+        AndroidJavaObject fileObject = new AndroidJavaObject("java.io.File", screenShotPath);
+        AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromFile", fileObject);
 
-            AndroidJavaObject fileObject = new AndroidJavaObject("java.io.File", screenShotPath);
-            AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromFile", fileObject);
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
 
-            intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
 
-            AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-
-            AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share Image");
-            currentActivity.Call("startActivity", jChooser);*/
+        AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share Image");
+        currentActivity.Call("startActivity", jChooser);*/
 
