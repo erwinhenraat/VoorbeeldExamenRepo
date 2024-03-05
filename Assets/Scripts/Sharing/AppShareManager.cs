2261166124395c00ec69a80c0e571aeab3249699
Minoord <70703952+Minoord@users.noreply.github.com>
2024-03-05 09:41:29 +0100

    Added summary and removes temp files

--- Assets/Scripts/Sharing/AppShareManager.cs
+++ Assets/Scripts/Sharing/AppShareManager.cs
@@ -18,7 +18,7 @@ namespace UntitledCube.Sharing
         {
             "I challenge you to beat my time of 1 in this IMPOSSIBLE seed: \n 23456 \n",
             "Think you can beat my time? I clocked 1 in this seed. Let's see what you've got! \n 23456 \n",
-            " 23456 \n Seed is mine – beaten in 1 . But I dare you to try and top it 😉 \n",
+            "23456 \n Seed is mine – beaten in 1 . But I dare you to try and top it 😉 \n",
             "Okay, hotshots. I just blazed through this seed in 1 . Your turn!  \n 23456 \n",
             "My fingers were FLYING. I nailed a 1 run in this seed. Show me your skills! \n 23456 \n",
             "1 seconds in this seed! It's your time to shine... or get crushed 😏 \n 23456 \n"
@@ -30,13 +30,29 @@ namespace UntitledCube.Sharing
             _stopwatch.OnTimerStopped += SetScoreTimer;
         }
 
+        /// <summary>
+        /// Checks if sharing isn't in procces, clears the temp folder and calls share screenshot
+        /// </summary>
         public void CallSharePopUp()
         {
             if (!_isProcessing)
+            {
+                RemoveAllTempFiles();
                 StartCoroutine(ShareScreenshotInAnroid());
+            }
         }
 
         private void SetScoreTimer(string timer) => _scoreTimer = timer;
+
+        private void RemoveAllTempFiles()
+        {
+            DirectoryInfo di = new DirectoryInfo(Path.Combine(Application.persistentDataPath));
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+        }
 
         private IEnumerator ShareScreenshotInAnroid()
         {
@@ -58,7 +74,7 @@ namespace UntitledCube.Sharing
             yield return new WaitForEndOfFrame();
 
             new NativeShare().AddFile(screenShotPath)
-            .SetSubject("Untitled Cube Highscore").SetText(message).SetUrl("\n\n https://TotallyARealLinkThatGoesToOurApp")
+            .SetSubject("Untitled Cube Highscore").SetText(message).SetUrl("\n\n https://github.com/swzwij/Proeve-van-Bekwaamheid-2023-2024/releases/tag/Sprint_1")
             .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
             .Share();
 
