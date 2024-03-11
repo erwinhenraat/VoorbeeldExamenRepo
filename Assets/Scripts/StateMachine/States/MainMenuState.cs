1ceb2a736914975b6588c402fdc0fce7f7255758
Minoord <70703952+Minoord@users.noreply.github.com>
2024-03-11 15:02:32 +0100

    Changed back build settings and Scene name from MainMenu

--- Assets/Scripts/StateMachine/States/MainMenuState.cs
+++ Assets/Scripts/StateMachine/States/MainMenuState.cs
@@ -4,19 +4,20 @@ namespace MarkUlrich.StateMachine.States
 {
     public class MainMenuState : State
     {
-        private const string SCENE_NAME = "TestShareSeed";
+        private const string SCENE_NAME = "MainMenu";
 
         public override void EnterState()
         {
             base.EnterState();
             LoadSceneAsync(SCENE_NAME, LoadSceneMode.Single);
             SetNextState<GameState>();
+            MoveToNextState();
         }
 
         public override void ExitState()
         {
             base.ExitState();
-            //UnloadScene(SCENE_NAME);
+            UnloadScene(SCENE_NAME);
         }
     }
 }
